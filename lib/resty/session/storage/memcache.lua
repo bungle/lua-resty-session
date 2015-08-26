36410744dee9a89c4e70cdf37ea9787754cc4eac
Hans Zandbelt <hzandbelt@pingidentity.com>
2015-08-26 11:07:58 +0100

    add distributed locking with memcache

--- lib/resty/session/storage/memcache.lua
+++ lib/resty/session/storage/memcache.lua
@@ -1,5 +1,4 @@
 local memcached    = require "resty.memcached"
-local lock         = require "resty.lock"
 local utils        = require "resty.session.utils"
 local split        = utils.split
 local decode       = utils.decode
@@ -10,31 +9,56 @@ local now          = ngx.now
 local shared       = ngx.shared
 local setmetatable = setmetatable
 local floor        = math.floor
-local locks        = ngx.var.session_memcache_sessions_locks or "sessions_locks"
+local uselocking   = ngx.var.session_memcache_uselocking or true
 local server       = ngx.var.session_memcache_server or "127.0.0.1"
 local port         = ngx.var.session_memcache_port or 11211
-
+local spinlockwait = ngx.var.session_memcache_spinlockwait or 10000
+local maxlockwait  = ngx.var.session_memcache_maxlockwait or 30
+local lockprefix   = ngx.var.session_memcache_lockprefix or "session-memcache-lock"
 local memcache = {}
 
 memcache.__index = memcache
 
+local function lock(m, i)
+    if uselocking then
+        for j = 0, (1000000 / spinlockwait) * maxlockwait do
+            local ok, err = m.memc:add(lockprefix .. "." .. encode(i), '1', maxlockwait+1)
+            if ok then
+                m.locked = true
+                return true, nil
+            end
+            ngx.sleep(spinlockwait / 1000000)
+        end
+        return false, "no lock"
+    end
+    return true, nil
+end
+
+function unlock(m, i)
+    if uselocking then
+        m.memc:delete(lockprefix .. "." .. encode(i))
+        m.locked = false
+    end
+    return true, nil
+end
+
 function memcache.new()
     local m, err = memcached:new()
     m:connect(server, port)
-    return setmetatable({ lock = lock:new(locks), memc = m }, memcache)
+    return setmetatable({ memc = m, locked = false }, memcache)
 end
 
 function memcache:open(cookie, lifetime)
     local r = split(cookie, "|", 3)
     if r and r[1] and r[2] and r[3] then
-        local i, e, h, l = decode(r[1]), tonumber(r[2]), decode(r[3]), self.lock
-        local ok, err = l:lock(i)
+        local i, e, h = decode(r[1]), tonumber(r[2]), decode(r[3])
+        local ok, err = lock(self, i)
         if ok then
             local d, flags, err = self.memc:get(encode(i))        
             if not err and d then
                 self.memc:set(encode(i), d, floor(lifetime))
             end
-            l:unlock()
+            unlock(self, i)
             return i, e, d, h
         end
         return nil, err
@@ -43,7 +67,7 @@ function memcache:open(cookie, lifetime)
 end
 
 function memcache:start(i)
-    self.lock:lock(i)
+    lock(self, i)
 end
 
 function memcache:save(i, e, d, h, close)
@@ -51,7 +75,7 @@ function memcache:save(i, e, d, h, close)
     if l > 0 then
         local ok, err = self.memc:set(encode(i), d, floor(l))
         if close then
-            self.lock:unlock()
+            unlock(self, i)
         end
         if ok then
             return concat({ encode(i), e, encode(h) }, "|")
@@ -59,14 +83,14 @@ function memcache:save(i, e, d, h, close)
         return nil, err
     end
     if close then
-        self.lock:unlock()
+        unlock(self, i)
     end
     return nil, "expired"
 end
 
 function memcache:destroy(i)
     self.memc:delete(encode(i))
-    self.lock:unlock()
+    unlock(self, i)
     self.memc:close()
 end
 
