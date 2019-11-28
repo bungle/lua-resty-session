375302cd95d2d88dc602ce1c48634ffd29be827f
Thijs Schreijer <thijs@thijsschreijer.nl>
2019-11-28 17:41:49 +0100

    chore(*) update code readability
    Adds comments, and more descriptive variable names

--- lib/resty/session/storage/shm.lua
+++ lib/resty/session/storage/shm.lua
@@ -55,71 +55,87 @@ function shm.new(config)
     return setmetatable(self, shm)
 end
 
-function shm:key(i)
-    return self.encode(i)
+function shm:key(id)
+    return self.encode(id)
 end
 
-function shm:cookie(c)
-    local r, d = {}, self.delimiter
-    local i, p, s, e = 1, 1, c:find(d, 1, true)
-    while s do
-        if i > 2 then
-            return nil
+-- Extracts the elements from the cookie-string (string-split essentially).
+-- @param value (string) the string to split in the elements
+-- @return array with the elements in order, or `nil` if the number of elements do not match expectations.
+function shm:cookie(value)
+    local result, delim = {}, self.delimiter
+    local count, pos = 1, 1
+    local match_start, match_end = value:find(delim, 1, true)
+    while match_start do
+        if count > 2 then
+            return nil  -- too many elements
         end
-        r[i] = c:sub(p, e - 1)
-        i, p = i + 1, e + 1
-        s, e = c:find(d, p, true)
+        result[count] = value:sub(pos, match_end - 1)
+        count, pos = count + 1, match_end + 1
+        match_start, match_end = value:find(delim, pos, true)
     end
-    if i ~= 3 then
-        return nil
+    if count ~= 3 then
+        return nil  -- too little elements (3 expected)
     end
-    r[3] = c:sub(p)
-    return r
+    result[3] = value:sub(pos)
+    return result
 end
 
-function shm:open(cookie, lifetime)
-    local r = self:cookie(cookie)
+-- Opens session and writes it to the store. Returns 4 decoded data elements from the cookie-string.
+-- @param value (string) the cookie string containing the encoded data.
+-- @param lifetime (number) lifetime in seconds of the data in the store (ttl)
+-- @return id (string), expires(number), data (string), hash (string).
+function shm:open(value, lifetime)
+    local r = self:cookie(value)
     if r and r[1] and r[2] and r[3] then
-        local i, e, h = self.decode(r[1]), tonumber(r[2]), self.decode(r[3])
-        local k = self:key(i)
+        local id, expires, hash = self.decode(r[1]), tonumber(r[2]), self.decode(r[3])
+        local key = self:key(id)
         if self.uselocking then
             local l = self.lock
-            local ok, err = l:lock(concat{k, ".lock"})
+            local ok, err = l:lock(concat{key, ".lock"})
             if ok then
-                local s = self.store
-                local d = s:get(k)
-                s:set(k, d, lifetime)
+                local cshm = self.store
+                local data = cshm:get(key)
+                cshm:set(key, data, lifetime)
                 l:unlock()
-                return i, e, d, h
+                return id, expires, data, hash
             end
             return nil, err
         else
-            local s = self.store
-            local d = s:get(k)
-            s:set(k, d, lifetime)
-            return i, e, d, h
+            local cshm = self.store
+            local data = cshm:get(key)
+            cshm:set(key, data, lifetime)
+            return id, expires, data, hash
         end
     end
     return nil, "invalid"
 end
 
-function shm:start(i)
+-- acquire locks if required
+function shm:start(id)
     if self.uselocking then
-        return self.lock:lock(concat{self:key(i), ".lock"})
+        return self.lock:lock(concat{self:key(id), ".lock"})
     end
     return true, nil
 end
 
-function shm:save(i, e, d, h, close)
-    local l = e - now()
-    if l > 0 then
-        local k = self:key(i)
-        local ok, err = self.store:set(k, d, l)
+-- Saves the session data to the SHM.
+-- server-side in this case.
+-- @param id (string)
+-- @param expires(number) lifetime in SHM (ttl) is calculated from this
+-- @param data (string)
+-- @param hash (string)
+-- @return encoded cookie-string value
+function shm:save(id, expires, data, hash, close)
+    local lifetime = expires - now()
+    if lifetime > 0 then
+        local key = self:key(id)
+        local ok, err = self.store:set(key, data, lifetime)
         if self.uselocking and close then
             self.lock:unlock()
         end
         if ok then
-            return concat({ k, e, self.encode(h) }, self.delimiter)
+            return concat({ key, expires, self.encode(hash) }, self.delimiter)
         end
         return nil, err
     end
@@ -129,25 +145,32 @@ function shm:save(i, e, d, h, close)
     return nil, "expired"
 end
 
-function shm:close(_)
+-- release any locks
+-- @return true
+function shm:close()
     if self.uselocking then
         self.lock:unlock()
     end
     return true
 end
 
-function shm:destroy(i)
-    self.store:delete(self:key(i))
+-- destroy the session by deleting is from the SHM
+-- @param id (string) id of session to destroy
+-- @return true
+function shm:destroy(id)
+    self.store:delete(self:key(id))
     if self.uselocking then
         self.lock:unlock()
     end
     return true, nil
 end
 
-function shm:ttl(i, ttl)
-  local k = self:key(i)
-  return self.store:expire(k, ttl)
+-- updates the remaining ttl in the SHM
+-- @param id (string) id of session to update
+-- @param lifetime (number) time in seconds the value should remain available
+function shm:ttl(id, lifetime)
+  local k = self:key(id)
+  return self.store:expire(k, lifetime)
 end
-
 
 return shm
--- lib/resty/session/strategies/default.lua
+++ lib/resty/session/strategies/default.lua
@@ -4,25 +4,35 @@ local concat = table.concat
 
 local default = {}
 
-function default:save(close)
-  local i, e, s = self.id, self.expires, self.storage
-  local k = self.hmac(self.secret, i .. e)
-  local d = self.serializer.serialize(self.data)
-  local h = self.hmac(k, concat{ i, e, d, self.key })
-  return s:save(i, e, self.cipher:encrypt(d, k, i, self.key), h, close)
+-- save the session data to the underlying storage adapter.
+-- @param session_obj (table) the session object to store
+-- @return result from `storage.save`.
+function default.save(session_obj, close)
+  local id, expires, storage = session_obj.id, session_obj.expires, session_obj.storage
+  local key = session_obj.hmac(session_obj.secret, id .. expires)
+  local data = session_obj.serializer.serialize(session_obj.data)
+  local hash = session_obj.hmac(key, concat{ id, expires, data, session_obj.key })
+
+  data = session_obj.cipher:encrypt(data, key, id, session_obj.key)
+  return storage:save(id, expires, data, hash, close)
 end
 
-function default:open(cookie)
-  local i, e, d, h = self.storage:open(cookie, self.cookie.lifetime)
-  if i and e and e > time() and d and h then
-    local k = self.hmac(self.secret, i .. e)
-    d = self.cipher:decrypt(d, k, i, self.key)
-    if d and self.hmac(k, concat{ i, e, d, self.key }) == h then
-      d = self.serializer.deserialize(d)
-      self.id = i
-      self.expires = e
-      self.data = type(d) == "table" and d or {}
-      self.present = true
+-- Calls into the underlying storage adapter to load the cookie.
+-- Validates the expiry-time and hash.
+-- @param session_obj (table) the session object to store the data in
+-- @param cookie (string) the cookie string to open
+-- @return `true` if ok, and will have set session properties; id, expires, data and present. Returns `nil` otherwise.
+function default.open(session_obj, cookie)
+  local id, expires, data, hash = session_obj.storage:open(cookie, session_obj.cookie.lifetime)
+  if id and expires and expires > time() and data and hash then
+    local key = session_obj.hmac(session_obj.secret, id .. expires)
+    data = session_obj.cipher:decrypt(data, key, id, session_obj.key)
+    if data and session_obj.hmac(key, concat{ id, expires, data, session_obj.key }) == hash then
+      data = session_obj.serializer.deserialize(data)
+      session_obj.id = id
+      session_obj.expires = expires
+      session_obj.data = type(data) == "table" and data or {}
+      session_obj.present = true
       return true
     end
   end
