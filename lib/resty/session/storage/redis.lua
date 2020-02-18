bungle/lua-resty-session

commit 1ecceee8dae3cba12d874d8173da22a305bc926f

the ttl function tries to use a not.connected redis socket (#85)
Signed-off-by: Stefan Bodewig <stefan.bodewig@innoq.com>

lib/resty/session/storage/redis.lua
@@ -249,7 +249,13 @@ end
 
 function redis:ttl(i, ttl)
   local k = self:key(i)
-  return self:expire(k, floor(ttl))
+  local ok, err = self:connect()
+  if ok then
+    local res, err = self:expire(k, floor(ttl))
+    self:set_keepalive()
+    return res, err
+  end
+  retunr nil, err
 end
 
 

commit 67c269e004e89f40994a5dffbdab47f0b2140454

fix typo introduced with #85
Signed-off-by: Stefan Bodewig <stefan.bodewig@innoq.com>

lib/resty/session/storage/redis.lua
@@ -255,7 +255,7 @@ function redis:ttl(i, ttl)
     self:set_keepalive()
     return res, err
   end
-  retunr nil, err
+  return nil, err
 end
 
 