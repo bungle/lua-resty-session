adfb528efb1dfb60fd4ce0acf154c8014a392b09
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2022-12-22 20:58:57 +0200

    add ldoc to utils

--- lib/resty/session/utils.lua
+++ lib/resty/session/utils.lua
@@ -40,6 +40,27 @@ local bpack, bunpack do
     return value
   end
 
+
+  ---
+  -- Binary pack unsigned integer.
+  --
+  -- Returns binary packed version of an integer in little endian unsigned format.
+  --
+  -- Size can be:
+  --
+  -- * `1`, pack input as a little endian unsigned char (`<C`)
+  -- * `2`, pack input as a little endian unsigned short (`<S`)
+  -- * `4`, pack input as a little endian unsigned integer (`<I`)
+  -- * `8`, pack input as a little endian unsigned long (`<L`)
+  --
+  -- @function utils.bpack
+  -- @tparam   number  size   size of binary packed output
+  -- @tparam   number  value  value to binary pack
+  -- @treturn  string         binary packed value
+  --
+  -- @usage
+  -- local packed_128 = require "resty.session.utils".bpack(1, 128)
+  -- local packed_now = require "resty.session.utils".bpack(8, ngx.time())
   bpack = function(size, value)
     if not binpack then
       binpack = require "lua_pack".pack
@@ -48,6 +69,30 @@ local bpack, bunpack do
     return bpack(size, value)
   end
 
+
+  ---
+  -- Binary unpack unsigned integer.
+  --
+  -- Returns number from a little endian unsigned binary packed format.
+  --
+  -- Size can be:
+  --
+  -- * `1`, unpack input from little endian unsigned char (`<C`)
+  -- * `2`, unpack input from little endian unsigned short (`<S`)
+  -- * `4`, unpack input from little endian unsigned integer (`<I`)
+  -- * `8`, unpack input from little endian unsigned long (`<L`)
+  --
+  -- @function utils.bunpack
+  -- @tparam   number  size   size of binary packed output
+  -- @tparam   number  value  value to binary pack
+  -- @treturn  number         binary unpacked value
+  --
+  -- @usage
+  -- local utils = require "resty.session.utils"
+  -- local value = 128
+  -- local packed_value = utils.bpack(1, value)
+  -- local unpacked_value = utils.bunpack(1, packed_value)
+  -- print(value == unpacked_value) -- true
   bunpack = function(size, value)
     if not binunpack then
       binunpack = require "lua_pack".unpack
@@ -66,6 +111,24 @@ local trim do
   local VTAB_BYTE  = byte("\v")
   local FF_BYTE    = byte("\f")
 
+  ---
+  -- Trim whitespace from the start and from the end of string.
+  --
+  -- Characters that are trimmed:
+  --
+  -- * space `" "`
+  -- * tab `"\t"`
+  -- * carriage return `"\r"`
+  -- * line feed `"\n"`
+  -- * vertical tab `"\v"`
+  -- * form feed `"\f"`
+  --
+  -- @function utils.trim
+  -- @tparam   string  value  string to trim
+  -- @treturn  string         a whitespace trimmed string
+  --
+  -- @usage
+  -- local trimmed = require "resty.session.utils".trim("  hello world  ")
   trim = function(value)
     if value == nil or value == "" then
       return ""
@@ -116,6 +179,16 @@ end
 
 local encode_json, decode_json do
   local cjson
+
+  ---
+  -- JSON encode value.
+  --
+  -- @function utils.encode_json
+  -- @tparam   any     value  value to json encode
+  -- @treturn  string         json encoded value
+  --
+  -- @usage
+  -- local json = require "resty.session.utils".encode_json({ hello = "world" })
   encode_json = function(value)
     if not cjson then
       cjson = require "cjson.safe".new()
@@ -123,6 +196,16 @@ local encode_json, decode_json do
     encode_json = cjson.encode
     return encode_json(value)
   end
+
+  ---
+  -- JSON decode value.
+  --
+  -- @function utils.decode_json
+  -- @tparam   string  value  string to json decode
+  -- @treturn  any            json decoded value
+  --
+  -- @usage
+  -- local tbl = require "resty.session.utils".decode_json('{ "hello": "world" }')
   decode_json = function(value)
     if not cjson then
       cjson = require "cjson.safe".new()
@@ -135,6 +218,15 @@ end
 
 local encode_base64url, decode_base64url, base64_size do
   local base64
+  ---
+  -- Base64 URL encode value.
+  --
+  -- @function utils.encode_base64url
+  -- @tparam   string  value  string to base64 url encode
+  -- @treturn  string         base64 url encoded value
+  --
+  -- @usage
+  -- local encoded = require "resty.session.utils".encode_base64url("test")
   encode_base64url = function(value)
     if not base64 then
       base64 = require "ngx.base64"
@@ -142,6 +234,18 @@ local encode_base64url, decode_base64url, base64_size do
     encode_base64url = base64.encode_base64url
     return encode_base64url(value)
   end
+
+  ---
+  -- Base64 URL decode value
+  --
+  -- @function utils.decode_base64url
+  -- @tparam   string  value  string to base64 url decode
+  -- @treturn  string         base64 url decoded value
+  --
+  -- @usage
+  -- local utils = require "resty.session.utils"
+  -- local encoded = utils.encode_base64url("test")
+  -- local decoded = utils.decode_base64url(encoded)
   decode_base64url = function(value)
     if not base64 then
       base64 = require "ngx.base64"
@@ -150,6 +254,16 @@ local encode_base64url, decode_base64url, base64_size do
     return decode_base64url(value)
   end
 
+  ---
+  -- Base64 size from original size (without padding).
+  --
+  -- @function utils.base64_size
+  -- @tparam   number  size  original size
+  -- @treturn  number        base64 url encoded size without padding
+  --
+  -- @usage
+  -- local test = "test"
+  -- local b64len = require "resty.session.utils".base64_size(#test)
   base64_size = function(size)
     return ceil(4 * size / 3)
   end
@@ -200,6 +314,17 @@ local deflate, inflate do
     return gzip(zlib.inflateGzip, data, DEFLATE_CHUNK_SIZE, DEFLATE_WINDOW_BITS)
   end
 
+
+  ---
+  -- Compress the data with deflate algorithm.
+  --
+  -- @function utils.deflate
+  -- @tparam   string  data  data to deflate
+  -- @treturn  string        deflated data
+  --
+  -- @usage
+  -- local test = "test"
+  -- local deflated = require "resty.session.utils".deflate(("a"):rep(100))
   deflate = function(data)
     if not zlib then
       zlib = require "ffi-zlib"
@@ -208,6 +333,17 @@ local deflate, inflate do
     return deflate(data)
   end
 
+  ---
+  -- Decompress the data compressed with deflate algorithm.
+  --
+  -- @function utils.inflate
+  -- @tparam   string  data  data to inflate
+  -- @treturn  string        inflated data
+  --
+  -- @usage
+  -- local utils = require "resty.session.utils"
+  -- local deflated = utils.deflate(("a"):rep(100))
+  -- local inflated = utils.inflate(deflated)
   inflate = function(data)
     if not zlib then
       zlib = require "ffi-zlib"
@@ -220,6 +356,17 @@ end
 
 local rand_bytes do
   local rand
+
+  ---
+  -- Generate crypto random bytes.
+  --
+  -- @function utils.rand_bytes
+  -- @tparam   number      length  how many bytes of random data to generate
+  -- @treturn  string|nil          random bytes
+  -- @treturn  string|nil          error message
+  --
+  -- @usage
+  -- local bytes = require "resty.session.utils".rand_bytes(32)
   rand_bytes = function(length)
     if not rand then
       rand = require "resty.openssl.rand"
@@ -257,6 +404,16 @@ local sha256 do
     return output
   end
 
+  ---
+  -- Calculates SHA-256 hash of the value.
+  --
+  -- @function utils.sha256
+  -- @tparam   string      value  value from which to calculate hash
+  -- @treturn  string|nil         sha-256 hash (32 bytes)
+  -- @treturn  string|nil         error message
+  --
+  -- @usage
+  -- local hash, err = require "resty.session.utils".sha256("hello world")
   sha256 = function(value)
     if not digest then
       digest = require "resty.openssl.digest"
@@ -299,6 +456,22 @@ local derive_hkdf_sha256 do
     return key
   end
 
+  ---
+  -- Derive a new key using HKDF with SHA-256.
+  --
+  -- @function utils.derive_hkdf_sha256
+  -- @tparam   string      ikm  initial key material
+  -- @tparam   string      nonce  nonce
+  -- @tparam   string      usage  e.g. `"encryption"` or `"authentication"`
+  -- @tparam   number      size   how many bytes to return
+  -- @treturn  string|nil         key material
+  -- @treturn  string|nil         error message
+  --
+  -- @usage
+  -- local utils = require "resty.session.utils"
+  -- local ikm = utils.rand_bytes(32)
+  -- local nonce = utils.rand_bytes(32)
+  -- local key, err = utils.derive_hkdf_sha256(ikm, nonce, "encryption", 32)
   derive_hkdf_sha256 = function(ikm, nonce, usage, size)
     if not kdf_derive then
       local kdf = require "resty.openssl.kdf"
@@ -346,6 +519,23 @@ local derive_pbkdf2_hmac_sha256  do
     return key
   end
 
+  ---
+  -- Derive a new key using PBKDF2 with SHA-256.
+  --
+  -- @function utils.derive_pbkdf2_hmac_sha256
+  -- @tparam   string      ikm    initial key material
+  -- @tparam   string      nonce  nonce
+  -- @tparam   string      usage  e.g. `"encryption"` or `"authentication"`
+  -- @tparam   number      size   how many bytes to return
+  -- @tparam   number      iterations  how many iterations to run, e.g. `100000`
+  -- @treturn  string|nil  key material
+  -- @treturn  string|nil  error message
+  --
+  -- @usage
+  -- local utils = require "resty.session.utils"
+  -- local ikm = utils.rand_bytes(32)
+  -- local nonce = utils.rand_bytes(32)
+  -- local key, err = utils.derive_pbkdf2_hmac_sha256(ikm, nonce, "encryption", 32, 100000)
   derive_pbkdf2_hmac_sha256 = function(ikm, nonce, usage, size, iterations)
     if not kdf_derive then
       local kdf = require "resty.openssl.kdf"
@@ -355,7 +545,6 @@ local derive_pbkdf2_hmac_sha256  do
         md = "sha256",
         pass = "",
         salt = "",
-
         pbkdf2_iter = 10000,
       }
       kdf_derive = kdf.derive
@@ -366,6 +555,28 @@ local derive_pbkdf2_hmac_sha256  do
 end
 
 
+---
+-- Derive a new AES-256 GCM-mode key and initialization vector.
+--
+-- Safety can be:
+-- * `nil` or `"None"`: key and iv will be derived using HKDF with SHA-256
+-- * `Low`: key and iv will be derived using PBKDF2 with SHA-256 (1.000 iterations)
+-- * `Medium`: key and iv will be derived using PBKDF2 with SHA-256 (10.000 iterations)
+-- * `High`: key and iv will be derived using PBKDF2 with SHA-256 (100.000 iterations)
+--
+-- @function utils.derive_aes_gcm_256_key_and_iv
+-- @tparam       string  ikm  initial key material
+-- @tparam       string  nonce   nonce
+-- @tparam[opt]  string  safety  safety of key generation
+-- @treturn      string|nil      key
+-- @treturn      string|nil      error message
+-- @treturn      string|nil      initialization vector
+--
+-- @usage
+-- local utils = require "resty.session.utils"
+-- local ikm = utils.rand_bytes(32)
+-- local nonce = utils.rand_bytes(32)
+-- local key, err, iv = utils.derive_aes_gcm_256_key_and_iv(ikm, nonce, "Medium")
 local function derive_aes_gcm_256_key_and_iv(ikm, nonce, safety)
   local bytes, err
   if safety and safety ~= "None" then
@@ -392,6 +603,20 @@ local function derive_aes_gcm_256_key_and_iv(ikm, nonce, safety)
 end
 
 
+---
+-- Derive HMAC SHA-256 key for message authentication using HDKF with SHA-256.
+--
+-- @function utils.derive_hmac_sha256_key
+-- @tparam   string  ikm    initial key material
+-- @tparam   string  nonce  nonce
+-- @treturn  string|nil     key
+-- @treturn  string|nil     error message
+--
+-- @usage
+-- local utils = require "resty.session.utils"
+-- local ikm = utils.rand_bytes(32)
+-- local nonce = utils.rand_bytes(32)
+-- local key, err = utils.derive_pbkdf2_hmac_sha256(ikm, nonce)
 local function derive_hmac_sha256_key(ikm, nonce)
   return derive_hkdf_sha256(ikm, nonce, "authentication", 32)
 end
@@ -420,6 +645,24 @@ local encrypt_aes_256_gcm, decrypt_aes_256_gcm do
     return cipher_aes_256_gcm:decrypt(key, iv, ciphertext, false, aad, tag)
   end
 
+  ---
+  -- Encrypt plain text using AES-256 in GCM-mode.
+  --
+  -- @function utils.encrypt_aes_256_gcm
+  -- @tparam   string  key  encryption key
+  -- @tparam   string  iv   initialization vector
+  -- @tparam   string  plaintext  plain text
+  -- @tparam   string  aad  additional authenticated data
+  -- @treturn  string|nil   ciphertext
+  -- @treturn  string|nil   error message
+  -- @treturn  string|nil   authentication tag
+  --
+  -- @usage
+  -- local utils = require "resty.session.utils"
+  -- local ikm = utils.rand_bytes(32)
+  -- local nonce = utils.rand_bytes(32)
+  -- local key, err, iv = utils.derive_aes_gcm_256_key_and_iv(ikm, nonce)
+  -- local enc, err, tag = utils.encrypt_aes_256_gcm(key, iv, "hello", "john@doe.com")
   encrypt_aes_256_gcm = function(key, iv, plaintext, aad)
     if not cipher_aes_256_gcm then
       cipher_aes_256_gcm = require("resty.openssl.cipher").new(AES_256_GCP_CIPHER)
@@ -428,6 +671,25 @@ local encrypt_aes_256_gcm, decrypt_aes_256_gcm do
     return encrypt_aes_256_gcm(key, iv, plaintext, aad)
   end
 
+  ---
+  -- Decrypt ciphertext using AES-256 in GCM-mode.
+  --
+  -- @function utils.decrypt_aes_256_gcm
+  -- @tparam   string  key  encryption key
+  -- @tparam   string  iv   initialization vector
+  -- @tparam   string  plaintext  plain text
+  -- @tparam   string  aad  additional authenticated data
+  -- @tparam   string  tag  authentication tag
+  -- @treturn  string|nil   ciphertext
+  -- @treturn  string|nil   error message
+  --
+  -- @usage
+  -- local utils = require "resty.session.utils"
+  -- local ikm = utils.rand_bytes(32)
+  -- local nonce = utils.rand_bytes(32)
+  -- local key, err, iv = utils.derive_aes_gcm_256_key_and_iv(ikm, nonce)
+  -- local enc, err, tag = utils.encrypt_aes_256_gcm(key, iv, "hello", "john@doe.com")
+  -- local out, err = utils.decrypt_aes_256_gcm(key, iv, ciphertext, "john@doe.com", tag)
   decrypt_aes_256_gcm = function(key, iv, ciphertext, aad, tag)
     if not cipher_aes_256_gcm then
       cipher_aes_256_gcm = require("resty.openssl.cipher").new(AES_256_GCP_CIPHER)
@@ -456,6 +718,21 @@ local hmac_sha256 do
     return digest
   end
 
+  ---
+  -- Calculate message authentication code with HMAC with SHA-256.
+  --
+  -- @function utils.hmac_sha256
+  -- @tparam   string  key    key
+  -- @tparam   string  value  value
+  -- @treturn  string|nil     message authentication code (32 bytes)
+  -- @treturn  string|nil     error message
+  --
+  -- @usage
+  -- local utils = require "resty.session.utils"
+  -- local ikm = utils.rand_bytes(32)
+  -- local nonce = utils.rand_bytes(32)
+  -- local key, err = utils.derive_hmac_sha256_key(ikm, nonce)
+  -- local mac, err = utils.hmac_sha256(key, "hello")
   hmac_sha256 = function(key, value)
     if not hmac then
       hmac = require "resty.openssl.hmac"
@@ -466,6 +743,21 @@ local hmac_sha256 do
 end
 
 
+---
+-- Loads session storage and creates a new instance using session configuration.
+--
+-- @function utils.load_storage
+-- @tparam  string  storage  name of the storage to load
+-- @tparam[opt]  string  configuration  session configuration
+-- @treturn  table|nil  instance of session storage
+-- @treturn  string|nil  error message
+--
+-- @usage
+-- local postgres = require "resty.session.utils".load_storage("postgres", {
+--   postgres = {
+--     host = "127.0.0.1",
+--   }
+-- })
 local function load_storage(storage, configuration)
   if storage == "file" then
     return require("resty.session.file").new(configuration and configuration.file)
@@ -500,6 +792,22 @@ local function load_storage(storage, configuration)
 end
 
 
+---
+-- Helper to format error messages.
+--
+-- @function utils.errmsg
+-- @tparam[opt]  string  err  a possible error coming from underlying library
+-- @tparam   string  msg  error message
+-- @tparam   any  ...  arguments for formatting the msg
+-- @treturn  string  formatted error message
+--
+-- @usage
+-- local utils = require "resty.session.utils"
+-- local test = "aaaa"
+-- local data, err = utils.deflate(test)
+-- if not data then
+--   print(utils.errmsg(err, "unable to deflate data '%s'", test)
+-- end
 local function errmsg(err, msg, ...)
   if select("#", ...) > 0 then
     msg = fmt(msg, ...)
@@ -513,6 +821,15 @@ local function errmsg(err, msg, ...)
 end
 
 
+---
+-- Helper to create a delimited key
+--
+-- @function utils.load_storage
+-- @tparam   table  self  a storage implementation
+-- @tparam   string  name  name
+-- @tparam   string  key  key
+-- @tparam   string  subject  subject
+-- @treturn  string  formatted and delimited name
 local function get_name(self, name, key, subject)
   local prefix = self.prefix
   local suffix = self.suffix
@@ -536,16 +853,56 @@ local function get_name(self, name, key, subject)
 end
 
 
+---
+-- Helper to turn on a flag.
+--
+-- @function utils.set_flag
+-- @tparam   number  options  options on which the flag is applied
+-- @tparam   number  flag  flag that is applied
+-- @treturn  number  options with the flag applied
+--
+-- @usage
+-- local options = 0x0000
+-- local FLAG_DOG = 0x001
+-- options = utils.set_flag(options, FLAG_DOG)
 local function set_flag(options, flag)
   return bor(options, flag)
 end
 
 
+---
+-- Helper to turn off a flag.
+--
+-- @function utils.unset_flag
+-- @tparam   number  options  options on which the flag is removed
+-- @tparam   number  flag  flag that is removed
+-- @treturn  number  options with the flag removed
+--
+-- @usage
+-- local options = 0x0000
+-- local FLAG_DOG = 0x001
+-- options = utils.set_flag(options, FLAG_DOG)
+-- options = utils.unset_flag(options, FLAG_DOG)
 local function unset_flag(options, flag)
   return band(options, bnot(flag))
 end
 
 
+---
+-- Helper to check if flag is enabled.
+--
+-- @function utils.has_flag
+-- @tparam   number  options  options on which the flag is checked
+-- @tparam   number  flag  flag that is checked
+-- @treturn  boolean  true if flag has is present, otherwise false
+--
+-- @usage
+-- local options = 0x0000
+-- local FLAG_DOG = 0x001
+-- local FLAG_BONE = 0x010
+-- options = utils.set_flag(options, FLAG_DOG)
+-- options = utils.set_flag(options, FLAG_BONE)
+-- print(utils.has_flag(options, FLAG_BONE)
 local function has_flag(options, flag)
   return band(options, flag) ~= 0
 end
