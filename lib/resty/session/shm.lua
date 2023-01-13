47811423cf69d4304c03a1f6b5eaa7f5ba6cd94e
samugi <samuele@konghq.com>
2023-01-13 16:21:18 +0100

    feat(storage-adapters):shm
    implement get,set,delete interface for shm

--- lib/resty/session/shm.lua
+++ lib/resty/session/shm.lua
@@ -4,13 +4,15 @@
 -- @module resty.session.shm
 
 
-local get_name = require "resty.session.utils".get_name
+local get_name    = require "resty.session.utils".get_name
+local collections = require "resty.session.scored-collections"
 
 
 local setmetatable = setmetatable
 local shared = ngx.shared
 local assert = assert
 local error = error
+local time = ngx.time
 
 
 local DEFAULT_ZONE = "sessions"
@@ -48,11 +50,51 @@ end
 -- @treturn true|nil ok
 -- @treturn string   error message
 function metatable:set(name, key, value, ttl, current_time, old_key, stale_ttl, metadata, remember)
+  if not metadata and not old_key then
+    local ok, err = self.dict:set(get_name(self, name, key), value, ttl)
+    if not ok then
+      return nil, err
+    end
+    return true
+  end
+
+  local old_name, old_ttl
+  if old_key then
+    old_name = get_name(self, name, old_key)
+    if not remember then
+      old_ttl  = self.dict:ttl(old_name)
+    end
+  end
+
   local ok, err = self.dict:set(get_name(self, name, key), value, ttl)
   if not ok then
     return nil, err
   end
-  return true
+
+  if old_name then
+    if remember then
+      self.dict:delete(old_name)
+    elseif (not old_ttl or old_ttl > stale_ttl) then
+      self.dict:expire(old_name, stale_ttl)
+    end
+  end
+
+  if metadata then
+    local audiences = metadata.audiences
+    local subjects  = metadata.subjects
+    for i = 1, #audiences do
+      local aud_sub_key = audiences[i] .. ":" .. subjects[i]
+      local exp_score   = (current_time or time()) - 1
+      local new_score   = (current_time or time()) + ttl
+
+      collections.remove_range_by_score(self, name, aud_sub_key, 0, exp_score)
+      collections.insert_element(self, name, aud_sub_key, key, new_score)
+      if old_key then
+        collections.delete_element(self, name, aud_sub_key, old_key)
+      end
+    end
+  end
+  return ok
 end
 
 
@@ -73,26 +115,6 @@ function metatable:get(name, key)
 end
 
 
--- TODO: needs to be removed (set command should do it)
-function metatable:ttl(name, key)
-  local ttl, err = self.dict:ttl(get_name(self, name, key))
-  if not ttl then
-    return nil, err
-  end
-  return ttl
-end
-
-
--- TODO: needs to be removed (set command should do it)
-function metatable:expire(name, key, ttl)
-  local ok, err = self.dict:expire(get_name(self, name, key), ttl)
-  if not ok then
-    return nil, err
-  end
-  return true
-end
-
-
 ---
 -- Delete session data.
 --
@@ -104,6 +126,19 @@ end
 -- @treturn string           error message
 function metatable:delete(name, key, metadata)
   self.dict:delete(get_name(self, name, key))
+  if not metadata then
+    return true
+  end
+
+  local audiences = metadata.audiences
+  local subjects  = metadata.subjects
+  local exp_score = time() - 1
+  for i = 1, #audiences do
+    local aud_sub_key = audiences[i] .. ":" .. subjects[i]
+    collections.remove_range_by_score(self, name, aud_sub_key, 0, exp_score)
+    collections.delete_element(self, name, aud_sub_key, key)
+  end
+
   return true
 end
 
